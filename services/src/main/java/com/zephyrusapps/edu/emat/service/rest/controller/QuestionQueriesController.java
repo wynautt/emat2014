d93876fc915547b199491348073a895a9947ad1b
wynaut <wynaut@undefined.com>
2014-02-06 00:28:34 +0000

    ADD integration with GAE (servlet-api 2.5)

--- services/src/main/java/com/zephyrusapps/edu/emat/service/rest/controller/QuestionQueriesController.java
+++ services/src/main/java/com/zephyrusapps/edu/emat/service/rest/controller/QuestionQueriesController.java
@@ -4,11 +4,12 @@ import com.zephyrusapps.edu.emat.service.rest.domain.Question;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 
-@RestController
+@Controller
 @RequestMapping("/emat")
 public class QuestionQueriesController {
 
@@ -16,6 +17,7 @@ public class QuestionQueriesController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
     public Question viewQuestion(@PathVariable() String id) {
         Question q = new Question(2012, "1", "1", "1", Arrays.asList("a","b","c","d"), id);
         return q;
@@ -23,6 +25,7 @@ public class QuestionQueriesController {
 
     @ExceptionHandler(NullPointerException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
     public String notFound() {
         return "not found";
     }
